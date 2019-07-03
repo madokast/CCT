f9fe4517f5543e008010f3c4a52d7cf4416d521e
madokast <578562554@qq.com>
2019-07-03 22:59:12 +0800

    wuwu

---
+++ src/zrx/base/Constants.java
@@ -0,0 +1,5 @@
+package zrx.base;
+
+public class Constants {
+    public static final double Miu0 = 4.0 * Math.PI * 1e-7;
+}
---
+++ src/zrx/base/Vector3d.java
@@ -0,0 +1,98 @@
+package zrx.base;
+
+public class Vector3d {
+    public double x;
+    public double y;
+    public double z;
+
+    public final static Vector3d corss(Vector3d a, Vector3d b){
+        return new Vector3d(a.y*b.z - a.z*b.y,
+                -a.x*b.z + a.z*b.x,
+                a.x*b.y - a.y*b.x);
+    }
+
+    public final static Vector3d add(Vector3d p0, Vector3d p1){
+        return new Vector3d(p0.x+p1.x, p0.y+p1.y, p0.z+p1.z);
+    }
+
+    public final static Vector3d subtract(Vector3d p0, Vector3d p1){
+        return new Vector3d(p0.x-p1.x, p0.y-p1.y, p0.z-p1.z);
+    }
+
+    public final static Vector3d dot(Vector3d p,double a){
+        return new Vector3d(p.x*a,p.y*a,p.z*a);
+    }
+
+    public final static Vector3d dot(double a,Vector3d p){
+        return new Vector3d(p.x*a,p.y*a,p.z*a);
+    }
+
+    public final static double dot(Vector3d a,Vector3d b){
+        return a.x*b.x+
+                a.y*b.y+
+                a.z*b.z;
+    }
+
+    public final void addSelf(Vector3d p){
+        x+=p.x;
+        y+=p.y;
+        z+=p.z;
+    }
+
+    public final void subtractSelf(Vector3d p){
+        x-=p.x;
+        y-=p.y;
+        z-=p.z;
+    }
+
+    public final double length(){
+        return Math.sqrt(x*x+y*y+z*z);
+    }
+
+    @Deprecated
+    public Vector3d(){
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+    }
+
+    public Vector3d(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    @Deprecated
+    public Vector3d(Vector3d p) {
+        this.x = p.x;
+        this.y = p.y;
+        this.z = p.z;
+    }
+
+    @Override
+    public String toString() {
+        return "Vector3d{" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
+    }
+
+    /**
+     * 测试
+     * @param args
+     */
+    public static void main(String[] args) {
+        Vector3d p1 = new Vector3d(1,1,1);
+        Vector3d p2 = new Vector3d(1,2,3);
+        System.out.println("p2 = " + p2);
+        System.out.println(p1.length());
+
+
+        System.out.println(add(p1, p2));
+        System.out.println(subtract(p1, p2));
+
+        p2.subtractSelf(p1);
+        System.out.println(p2);
+    }
+}
